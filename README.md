221a2eefd05a03e17493f2466732bcae0f7484bd
Alexandr Soldatov <53903142+SmashFlashDash@users.noreply.github.com>
2023-06-03 18:18:44 +0300

    добавлен build для server fat jar

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # test-javafx-databaseClient
+
+запустить server.jar  
+`java -jar -D"server.port=8888"  .\server-1.0.jar`
--- server/src/main/java/org/dbclient/server/ServerApp.java
+++ server/src/main/java/org/dbclient/server/ServerApp.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 @EntityScan(basePackages = {"common.data"})
 public class ServerApp {
     public static void main(String[] args) {
-        System.out.println(org.hibernate.Version.getVersionString());
         SpringApplication.run(ServerApp.class);
     }
 }
